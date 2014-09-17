bc1c9daf6ab5524a13adfae67ac5f801a00679df
Jessica Rosenkrantz <jessica@n-e-r-v-o-u-s.com>
2014-09-17 11:03:15 -0400

    dimensions > editable text boxes
    still need to impose limits on values

--- gui.js
+++ gui.js
@@ -7,13 +7,35 @@ initializes the GUI and its listeners
 called by main's init()
 */
 function init() {
+	console.log("starting up");
   var widthSlider = document.getElementById("widthSlider");
   var heightSlider = document.getElementById("heightSlider");
   var depthSlider = document.getElementById("depthSlider");
+  var widthOut = document.getElementById("widthOut");
+  var heightOut = document.getElementById("heightOut");
+  var depthOut = document.getElementById("depthOut");
   
   widthSlider.addEventListener("input", function() {bookshelf.setWidth(parseFloat(this.value));setBookshelfWidthUI();}, false);
   heightSlider.addEventListener("input", function() {bookshelf.setHeight(parseFloat(this.value));setBookshelfHeightUI();}, false);
   depthSlider.addEventListener("input", function() {bookshelf.setDepth(parseFloat(this.value));setBookshelfDepthUI();}, false);
+  
+  widthOut.addEventListener("change", function() {
+	var val = parseFloat(this.value)*25.4;
+	widthSlider.value = val;
+	bookshelf.setWidth(val);
+	setBookshelfWidthUI();}, false);
+
+  heightOut.addEventListener("change", function() {
+	var val = parseFloat(this.value)*25.4;
+	heightSlider.value = val;
+	bookshelf.setHeight(val);
+	setBookshelfHeightUI();}, false);
+	
+ depthOut.addEventListener("change", function() {
+	var val = parseFloat(this.value)*25.4;
+	depthSlider.value = val;
+	bookshelf.setDepth(val);
+	setBookshelfDepthUI();}, false);
   
   setBookshelfWidthUI();
   setBookshelfHeightUI();
@@ -28,8 +50,7 @@ callback for width slider
 function setBookshelfWidthUI() {
 	var wDiv = document.getElementById("widthOut");
 	var inches = bookshelf.width/25.4;
-	wDiv.innerHTML = parseFloat(inches).toFixed(1) + " in";
-	
+	wDiv.value = parseFloat(inches).toFixed(1);	
 }
 /*
 sets UI elements related to the bookshelf's height
@@ -39,7 +60,7 @@ callback for height slider
 function setBookshelfHeightUI(){
 	var hDiv = document.getElementById("heightOut");
 	var inches = bookshelf.height/25.4;
-	hDiv.innerHTML = parseFloat(inches).toFixed(1) + " in";
+	hDiv.value = parseFloat(inches).toFixed(1);
 };
 /*
 sets UI elements related to the bookshelf's depth
@@ -49,7 +70,7 @@ callback for depth slider
 function setBookshelfDepthUI(){
 	var hDiv = document.getElementById("depthOut");
 	var inches = bookshelf.depth/25.4;
-	hDiv.innerHTML = parseFloat(inches).toFixed(1) + " in";
+	hDiv.value = parseFloat(inches).toFixed(1);
 };
 
 function setNumCellsUI() {
